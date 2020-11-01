018a9b79e57dad8dad2eccc8530a87ca7c42836f
August Dolan <august.dolan1@gmail.com>
2020-11-01 09:45:15 -0800

    get me outta here

--- server/index.js
+++ server/index.js
@@ -1,22 +1,38 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const PORT = 3000;
 const path = require('path');
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'client', 'dist', 'index.html'))
-})
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')))
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   console.log('a user connected');
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  })
-  socket.on('disconnect', () => {
+
+  socket.on('disconnect', reason => {
     console.log('user disconnected');
-  })
-})
+  });
+
+  socket.on('room', data => {
+    console.log('room join');
+    console.log(data);
+    socket.join(data.room);
+  });
+
+  socket.on('leave room', data => {
+    console.log('leaving room');
+    console.log(data);
+    socket.leave(data.room)
+  });
+
+  socket.on('new message', data => {
+    console.log(data.room);
+    socket.broadcast
+    .to(data.room)
+    .emit('receive message', data)
+  });
+});
 
 http.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`)
